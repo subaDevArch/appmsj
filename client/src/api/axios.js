0bda34ab67fb77c46e380d8b4eaf2e5517f00213
subadevarch <franciscojinojosa@gmail.com>
2024-04-29 17:18:39 -0300

    empezndo a subir el front

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  //baseURL: "http://localhost:3000/api",
+  baseURL: "https://backend-msj.onrender.com/api",
   withCredentials : true
 
 });
