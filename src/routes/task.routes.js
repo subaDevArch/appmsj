93a6958d1f1d3c00253acb97611e3d27ef081d00
subadevarch <franciscojinojosa@gmail.com>
2024-04-29 17:50:45 -0300

    quito auth a task

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -6,7 +6,7 @@ import { createTaskSchema } from '../schemas/task.schema.js'
 
 const router = Router();
 
-router.get('/tasks',authRequired,getTasks)
+router.get('/tasks',getTasks)
 router.get('/tasks/:id',authRequired,getTask)
 router.post('/tasks',authRequired,validateSchema(createTaskSchema),createTask)
 router.delete('/tasks/:id',authRequired,deleteTask)
