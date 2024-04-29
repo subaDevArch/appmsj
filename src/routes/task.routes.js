fe7949435e98da67cbc5f16feae5f9260df7cec7
subadevarch <franciscojinojosa@gmail.com>
2024-04-29 19:06:49 -0300

    arreglando auth ya arrelge el envio de msjs

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,7 +7,7 @@ import { createTaskSchema } from '../schemas/task.schema.js'
 const router = Router();
 
 router.get('/tasks',getTasks)
-router.get('/tasks/:id',authRequired,getTask)
+router.get('/tasks/:id',getTask)
 router.post('/tasks',authRequired,validateSchema(createTaskSchema),createTask)
 router.delete('/tasks/:id',authRequired,deleteTask)
 router.put('/tasks/:id',authRequired,updateTask)
