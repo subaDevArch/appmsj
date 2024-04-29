c6421e5a218c3527b76564361642ce0037ac04e4
subadevarch <franciscojinojosa@gmail.com>
2024-04-29 18:54:46 -0300

    arreglando auth ya arrelge el envio de msjs

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+/*import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = (req, res, next) => {
@@ -21,4 +21,36 @@ export const authRequired = (req, res, next) => {
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
+};*/
+
+import jwt from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
+
+export const authRequired = (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      console.log("No se recibió ningún token en la solicitud");
+      return res.status(401).json({
+        message: "No token!, Acceso denegado!",
+      });
+    }
+
+    jwt.verify(token, TOKEN_SECRET, (error, user) => {
+      if (error) {
+        console.error("Error al verificar el token:", error.message);
+        return res.status(401).json({
+          message: "Invalid Token",
+        });
+      }
+      req.user = user;
+      console.log("Token verificado correctamente. Usuario:", user);
+      next(); // Llama a next() aquí para continuar con el siguiente middleware o controlador
+    });
+  } catch (error) {
+    console.error("Error en el middleware authRequired:", error.message);
+    return res.status(500).json({ message: error.message });
+  }
 };
+
