e65b93a4ebcfd17ef6893203e9b28e583c1d0116
subadevarch <franciscojinojosa@gmail.com>
2024-05-11 13:57:49 -0300

    mejorando interfaz

--- client/src/components/Inicio.jsx
+++ client/src/components/Inicio.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function Inicio() {
   return (
-    <div>Inicio</div>
-  )
+    <div className="m-4">
+      <div className="flex justify-center bg-gradient-to-r from-green-700 to-gray-600 rounded-md shadow-xl shadow-gray-400">
+        <h1 className="font-orbitron text-white text-3xl self-center m-4">
+          PRECEPTOR 2.00
+        </h1>
+      </div>
+      <hr className="my-4" />
+      <div className="text-center">
+        <p className="text-lg">¡Bienvenido a PRECEPTOR 2.00!</p>
+        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+      </div>
+      <hr className="my-4" />
+      <div className="my-8">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">Noticias Preceptoria</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {/* Aquí puedes colocar los elementos destacados */}
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <h3 className="text-lg font-semibold mb-2">Destacado 1</h3>
+            <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <h3 className="text-lg font-semibold mb-2">Destacado 2</h3>
+            <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <h3 className="text-lg font-semibold mb-2">Destacado 3</h3>
+            <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Inicio
+export default Inicio;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'orbitron': ['Orbitron', 'sans-serif'],
+        'noto': ['Noto Sans Thai', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
