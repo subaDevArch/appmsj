f3c7bbf37b2ebacc1c4205a5e7d9403b2a982aef
subadevarch <franciscojinojosa@gmail.com>
2024-05-12 14:40:43 -0300

    agregando tareas

--- client/src/components/Aplicaciones.jsx
+++ client/src/components/Aplicaciones.jsx
@@ -120,12 +120,7 @@ function Aplicaciones() {
         </div>
         <span className="text-xs mt-2 ml-2.5">Calendario Escolar</span>
       </a>
-      <Link to="/add-task" className="flex flex-col items-center">
-        <div className="bg-white hover:bg-gray-300 text-blue-500 font-bold py-8 px-8 rounded-xl shadow-md flex items-center justify-center">
-          <Mail className="w-9 h-9" />
-        </div>
-        <span className="text-xs mt-2">Tareas</span>
-      </Link>
+     
      
      
     </>
