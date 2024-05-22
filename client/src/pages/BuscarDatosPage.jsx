9fb895dc465a9db21067f2761fc186f81ed47cc0
subaDevArch <dev.arch.fi@gmail.com>
2024-05-22 17:00:38 -0300

    cambios interfaz

--- client/src/pages/BuscarDatosPage.jsx
+++ client/src/pages/BuscarDatosPage.jsx
@@ -4,6 +4,12 @@ import TablaDatosAlumnos from '../components/TablaDatosAlumnos'
 function BuscarDatosPage() {
   return (
     <div className="h-screen mb-16">
+       <div className="flex flex-col items-center justify-center bg-gradient-to-r from-green-700 to-gray-600 rounded-2xl rounded-t-none shadow-xl shadow-gray-400 p-3 text-center">
+  <h1 className=" text-white text-2xl font-bold font-orbitron mb-1">Datos ALumnos</h1> {/* Añadido un margen inferior */}
+  {isAuthenticated && user && (
+    <p className="text-white text-sm">Bienvenido, {user.username}!</p>
+  )}
+</div>
       <div><TablaDatosAlumnos/></div>
     </div>
     
