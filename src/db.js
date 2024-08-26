ac823d7f54357ad6ca6da10d57a1d515189dfe71
subaDevArch <148784443+subaDevArch@users.noreply.github.com>
2024-08-26 08:40:51 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ dotenv.config({path:'/home/idecom/app/src/env/.env'});
 export const connectDB = async () => {
     try {
         
-        await mongoose.connect("mongodb://localhost:27017/merndb")
+        await mongoose.connect("//mongodb+srv://peto:Peto34697936@cluster0.qp844en.mongodb.net/merndb")
         console.log('---DB IS CONECTED---')
     } catch (error) {
         console.log(error);
